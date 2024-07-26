9eed813816bf3ab7c2d11a470df39b338a499053
seifeldin15 <seifeldin02@outlook.com>
2024-07-26 03:05:51 +0300

    ADGSD

--- src/components/SliderTop/SliderTop.jsx
+++ src/components/SliderTop/SliderTop.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect, memo } from 'react';
 import './SliderTop.css';
 
-const DURATION = 30000;
+const DURATION = 20000;
 
 const InfiniteLoopSlider = memo(({ children, duration, direction }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
